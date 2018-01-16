2025e16f5fbed07c2357d6bdf3aa7a9854ed4ff6
Sergey Kardash <kardashsergeydev@gmail.com>
2018-01-16 15:28:40 +0200

    POST, PUT, DELETE requests with es5

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 const http = new easyHTTP;
 
 // Get Posts
+// http.get ('https://jsonplaceholder.typicode.com/posts', function (err, posts){
+//   if (err) {
+//     console.log (err)
+//   } else {
+//     console.log ( posts)
+//   }
+// });
 
-http.get ('https://jsonplaceholder.typicode.com/posts1', function (err, posts){
+// Get single post
+// http.get ('https://jsonplaceholder.typicode.com/posts/1', function (err, posts){
+//   if (err) {
+//     console.log (err)
+//   } else {
+//     console.log ( posts)
+//   }
+// });
+
+//  Post Request
+  // Create Data
+  const data = {
+    title: 'Custom Post',
+    body: 'This is custom post'
+  }
+
+  // http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, posts){
+  //     if (err) {
+  //       console.log (err)
+  //     } else {
+  //       console.log ( posts)
+  //     }
+  //   })
+
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function (err, posts){
+//   if (err) {
+//     console.log (err)
+//   } else {
+//     console.log ( posts)
+//   }
+// })
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/51', function (err, posts){
   if (err) {
     console.log (err)
   } else {
     console.log ( posts)
   }
-});
+})
